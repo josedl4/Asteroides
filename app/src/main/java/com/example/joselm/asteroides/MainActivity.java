9574f0d6a64968d4162be83b2b05acbf5f290f1d
Jose Luis Martín <joseop21@gmail.com>
2016-09-08 14:25:19 +0200

    Añadimos menu a la actividad principla, añadiendo las opciones de 'Acerca De' y 'Configuracion'.

--- app/src/main/java/com/example/joselm/asteroides/MainActivity.java
+++ app/src/main/java/com/example/joselm/asteroides/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.joselm.asteroides;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -38,5 +40,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void salir(View view){
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        if(id == R.id.acercaDe){
+            lanzarAcercaDe(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
