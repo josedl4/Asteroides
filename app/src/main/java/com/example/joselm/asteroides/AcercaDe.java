63643a8e26e121969d0db66bc3d9d73eba749051
Jose Luis Martín <joseop21@gmail.com>
2016-09-08 13:58:47 +0200

    Añadimos nueva actividad para la funcion 'Acerca De' de la actividad principal.

---
+++ app/src/main/java/com/example/joselm/asteroides/AcercaDe.java
@@ -0,0 +1,15 @@
+package com.example.joselm.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by joselm on 8/09/16.
+ */
+public class AcercaDe extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/com/example/joselm/asteroides/MainActivity.java
+++ app/src/main/java/com/example/joselm/asteroides/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.joselm.asteroides;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bAcercaDe = (Button) findViewById(R.id.but_acerca);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
+    }
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
     }
 }
